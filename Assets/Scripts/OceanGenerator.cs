ce7bebcd5510f884ba0722d9f4a87b443b11efd6
Saulius Vincevičius <sauliusvince@gmail.com>
2023-11-15 17:32:00 +0000

    Did I just make height map?

--- Assets/Scripts/OceanGenerator.cs
+++ Assets/Scripts/OceanGenerator.cs
@@ -22,6 +22,9 @@ public class OceanGenerator : MonoBehaviour
     //[SerializeField] private float _peakEnhancementFactor = 3.3f;
     [SerializeField] private float _depth = 1000f;
     
+    // Outputs
+    RenderTexture _spectrumMap;
+    
     private MeshFilter _meshFilter;
     private MeshRenderer _meshRenderer;
     private Texture2D _gaussianNoise;
@@ -52,9 +55,10 @@ public class OceanGenerator : MonoBehaviour
     private void InitSpectrumMap()
     {
         // Create a new RenderTexture
-        RenderTexture renderTexture = new RenderTexture(_size, _size, 24);
-        renderTexture.enableRandomWrite = true;
-        renderTexture.Create();
+        _spectrumMap = new RenderTexture(_size, _size, 24, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear);
+        _spectrumMap.enableRandomWrite = true;
+        _spectrumMap.Create();
+
         
         // Find kernel
         int kernel = _computeShader.FindKernel("CalculateSpectrum");
@@ -70,31 +74,101 @@ public class OceanGenerator : MonoBehaviour
         _computeShader.SetTexture(0, "Noise", _gaussianNoise);
 
         // Bind the RenderTexture to the compute shader
-        _computeShader.SetTexture(kernel, "SpectrumMap", renderTexture);
+        _computeShader.SetTexture(kernel, "SpectrumMap", _spectrumMap);
 
         // Dispatch the compute shader
         _computeShader.Dispatch(kernel, _size / 8, _size / 8, 1);
-
-        // Set the texture of the RawImage to the RenderTexture
-        _spectrumMapImage.texture = renderTexture;
+        
+        _spectrumMapImage.texture = _spectrumMap;
+        PrintSpectrumTexture(_spectrumMap);
     }
+
+    private void PrintSpectrumTexture(RenderTexture renderTexture)
+    {
+        // Create a new Texture2D with a format that supports negative values
+        Texture2D texture2D = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBAHalf, false);
+
+        // Set the active RenderTexture
+        RenderTexture.active = renderTexture;
+
+        // Read the pixels from the RenderTexture
+        texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+
+        // Apply the changes
+        texture2D.Apply();
+
+        // Reset the active RenderTexture
+        RenderTexture.active = null;
+
+        // Now you have a Texture2D object with the data from the RenderTexture
+        // You can access the pixel data, keeping in mind that the values can be negative
+
+        Vector3 lowestX = new Vector3(0, 0, 0);
+        Vector3 highestX = new Vector3(0, 0, 0);
+        Vector3 lowestY = new Vector3(0, 0, 0);
+        Vector3 highestY = new Vector3(0, 0, 0);
+
+        // Print the pixel values
+        for (int i = 0; i < texture2D.width; i++)
+        {
+            for (int j = 0; j < texture2D.height; j++)
+            {
+                Color pixelColor = texture2D.GetPixel(i, j);
+                Debug.Log("Pixel (" + i + ", " + j + "): " + pixelColor);
+
+                if (pixelColor.r < lowestX.x)
+                {
+                    lowestX.x = pixelColor.r;
+                    lowestX.y = i;
+                    lowestX.z = j;
+                } 
+                
+                if (pixelColor.r > highestX.x)
+                {
+                    highestX.x = pixelColor.r;
+                    highestX.y = i;
+                    highestX.z = j;
+                }
+                
+                if (pixelColor.g < lowestY.x)
+                {
+                    lowestY.x = pixelColor.g;
+                    lowestY.y = i;
+                    lowestY.z = j;
+                }
+                
+                if (pixelColor.g > highestY.x)
+                {
+                    highestY.x = pixelColor.g;
+                    highestY.y = i;
+                    highestY.z = j;
+                }
+            }
+        }
+        
+        print($"Lowest X: {lowestX.x} at ({lowestX.y}, {lowestX.z})");
+        print($"Highest X: {highestX.x} at ({highestX.y}, {highestX.z})");
+        print($"Lowest Y: {lowestY.x} at ({lowestY.y}, {lowestY.z})");
+        print($"Highest Y: {highestY.x} at ({highestY.y}, {highestY.z})");
+    }
+
 
     private void GenerateHightMap()
     {
         // Create a new RenderTexture
-        RenderTexture renderTexture = new RenderTexture(_size, _size, 24);
+        RenderTexture renderTexture = new RenderTexture(_size, _size, 24, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear);
         renderTexture.enableRandomWrite = true;
         renderTexture.Create();
     
         // Find kernel
         int kernel = _computeShader.FindKernel("CalculateHeight");
-    
+        
         // Set constants
         _computeShader.SetInt("Size", _size);
         _computeShader.SetInt("LengthScale", _lengthScale);
         
         // Set SpectrumMap texture for CalculateHeight kernel
-        _computeShader.SetTexture(kernel, "SpectrumMap", _spectrumMapImage.texture);
+        _computeShader.SetTexture(kernel, "SpectrumMap", _spectrumMap);
 
         // Bind the RenderTexture to the compute shader
         _computeShader.SetTexture(kernel, "HeightMap", renderTexture);
@@ -104,8 +178,8 @@ public class OceanGenerator : MonoBehaviour
     
         // Set the texture of the RawImage to the RenderTexture
         _hightMapImage.texture = renderTexture;
+        //PrintSpectrumTexture(renderTexture);
     }
-
 
     private void GeneratePlane(int size, int resolution)
     {
@@ -148,16 +222,17 @@ public class OceanGenerator : MonoBehaviour
         _meshFilter.mesh = mesh;
     }
     
+    //Code from https://stackoverflow.com/a/218600
     Texture2D GenerateGaussianNoise(int size)
     {
-        Texture2D noise = new Texture2D(size, size, TextureFormat.RGFloat, false, true);
+        Texture2D noise = new Texture2D(size, size, TextureFormat.RGBAFloat, false, true);
         noise.filterMode = FilterMode.Point;
         
         for (int i = 0; i < size; i++)
         {
             for (int j = 0; j < size; j++)
             {
-                noise.SetPixel(i, j, new Vector4(NormalRandom(), NormalRandom()));
+                noise.SetPixel(i, j, new Vector4(NormalRandom(), NormalRandom(), NormalRandom(), NormalRandom()));
             }
         }
         
